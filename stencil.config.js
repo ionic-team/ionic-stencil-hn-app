ce4c059d640aa87729d2dbc296163e5e01ba93a5
jgw96 <jgw9617@gmail.com>
2017-07-06 15:10:06 -0500

    refactor(): wip to new structure

---
+++ stencil.config.js
@@ -0,0 +1,10 @@
+exports.config = {
+  src: 'src',
+  dest: 'dist',
+  bundles: [
+    { components: ['news-list', 'news-container', 'comments-page', 'comments-list'] }
+  ],
+  collections: [
+    '@ionic/core',
+  ]
+};
