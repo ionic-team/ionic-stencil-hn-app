383ff3cba92db6e4782975b10b5185867b83a77f
jgw96 <jgw9617@gmail.com>
2017-08-04 11:19:23 -0500

    chore(): update to latest packages and stencil-config structure

--- stencil.config.js
+++ stencil.config.js
@@ -1,5 +1,13 @@
 exports.config = {
   bundles: [
     { components: ['news-list', 'news-container', 'comments-page', 'comments-list'] }
+  ],
+  collections: [
+    { name: '@ionic/core' }
   ]
 };
+
+exports.devServer = {
+  root: 'www',
+  watchGlob: '**/**'
+}
