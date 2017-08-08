c2a937fece62e1fd30bc3f1a1982a4cd14501e29
jgw96 <jgw9617@gmail.com>
2017-08-08 11:22:20 -0500

    feat(): make fakeFetch a component

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,20 +1,19 @@
-import { Component, State } from '@stencil/core';
-import { fakeFetch } from '../../utils'
-
+import { Component, State, Element } from '@stencil/core';
 
 @Component({
   tag: 'news-container',
   styleUrl: '../main.scss'
 })
 export class NewsContainer {
-
+  
+  @Element() el: HTMLElement;
   page: number;
   pageType: string;
   apiRootUrl: string = 'https://node-hnapi.herokuapp.com';
-
+  
   @State() stories: any[];
 
-  ionViewWillLoad() {
+  ionViewDidLoad() {
     // call to firebase function for first view
     this.getStories('news', 1);
   }
@@ -23,102 +22,106 @@ export class NewsContainer {
   getStories(type: string, pageNum: number = 1) {
     this.page = pageNum;
     this.pageType = type;
-    fakeFetch(`${this.apiRootUrl}/${this.pageType}?page=${this.page}`).then((data) => {
+
+    const fakeFetch: any = this.el.querySelector('fake-fetch');
+
+    fakeFetch.fetch(`${this.apiRootUrl}/${this.pageType}?page=${this.page}`).then((data) => {
       this.stories = data;
     }).catch((err) => {
       console.error('Could not load data', err);
-    })
+    });
   }
 
   render() {
     console.log('rendering');
 
-      return [
-        <ion-header mdHeight='56px' iosHeight='61px'>
-          <ion-toolbar color='primary'>
+    return [
+      <ion-header mdHeight='56px' iosHeight='61px'>
+        <ion-toolbar color='primary'>
 
-            <ion-buttons slot='left'>
-              <ion-icon name='ionic'></ion-icon>
-            </ion-buttons>
+          <ion-buttons slot='left'>
+            <ion-icon name='ionic'></ion-icon>
+          </ion-buttons>
 
-            <div class='tabs-bar'>
-              <ion-button
-                class={{
-                  'header-button': true,
-                  'first-button': true,
-                  'header-button-selected': this.pageType === 'news'
-                }}
-                clear
-                onClick={() => this.getStories('news', 1)}
-              >
-                News
+          <div class='tabs-bar'>
+            <ion-button
+              class={{
+                'header-button': true,
+                'first-button': true,
+                'header-button-selected': this.pageType === 'news'
+              }}
+              clear
+              onClick={() => this.getStories('news', 1)}
+            >
+              News
               </ion-button>
-              <ion-button
-                class={{
-                  'header-button': true,
-                  'header-button-selected': this.pageType === 'show'
-                }}
-                clear
-                onClick={() => this.getStories('show', 1)}
-              >
-                Show
+            <ion-button
+              class={{
+                'header-button': true,
+                'header-button-selected': this.pageType === 'show'
+              }}
+              clear
+              onClick={() => this.getStories('show', 1)}
+            >
+              Show
               </ion-button>
-              <ion-button
-                class={{
-                  'header-button': true,
-                  'header-button-selected': this.pageType === 'jobs'
-                }}
-                clear
-                onClick={() => this.getStories('jobs', 1)}
-              >
-                Jobs
+            <ion-button
+              class={{
+                'header-button': true,
+                'header-button-selected': this.pageType === 'jobs'
+              }}
+              clear
+              onClick={() => this.getStories('jobs', 1)}
+            >
+              Jobs
               </ion-button>
-              <ion-button
-                class={{
-                  'header-button': true,
-                  'header-button-selected': this.pageType === 'ask'
-                }}
-                clear
-                onClick={() => this.getStories('ask', 1)}
-              >
-                Ask
+            <ion-button
+              class={{
+                'header-button': true,
+                'header-button-selected': this.pageType === 'ask'
+              }}
+              clear
+              onClick={() => this.getStories('ask', 1)}
+            >
+              Ask
               </ion-button>
-            </div>
-          </ion-toolbar>
-        </ion-header>,
+          </div>
+        </ion-toolbar>
+      </ion-header>,
 
-        (this.stories !== undefined) ?
+      (this.stories !== undefined) ?
         <ion-content>
-          <news-list story-list={this.stories}>
+          <news-list storyList={this.stories}>
           </news-list>
         </ion-content>
         : null,
 
-        <ion-footer>
-          <ion-toolbar class='pager'>
-            <ion-buttons slot='start'>
-              <ion-button
-                class={{ 'no-back': this.page === 1,  'yes-back': this.page === 1 }}
-                clear={true}
-                onclick={() => this.getStories(this.pageType, this.page - 1)}
-              >
-                prev
+      <ion-footer>
+        <ion-toolbar class='pager'>
+          <ion-buttons slot='start'>
+            <ion-button
+              class={{ 'no-back': this.page === 1, 'yes-back': this.page === 1 }}
+              clear={true}
+              onclick={() => this.getStories(this.pageType, this.page - 1)}
+            >
+              prev
               </ion-button>
-            </ion-buttons>
-            <span class='page-number'>
-              page {this.page}
-            </span>
-            <ion-buttons slot='end'>
-              <ion-button
-                clear={true}
-                onclick={() => this.getStories(this.pageType, this.page + 1)}
-              >
-                next
+          </ion-buttons>
+          <span class='page-number'>
+            page {this.page}
+          </span>
+          <ion-buttons slot='end'>
+            <ion-button
+              clear={true}
+              onclick={() => this.getStories(this.pageType, this.page + 1)}
+            >
+              next
               </ion-button>
-            </ion-buttons>
-          </ion-toolbar>
-        </ion-footer>
-      ];
-    }
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-footer>,
+      
+      <fake-fetch></fake-fetch>
+    ];
   }
 }
